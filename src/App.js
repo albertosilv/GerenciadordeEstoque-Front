a3fdb861b105bde9017c9ece3ff9318dde6d4650
Alberto <alberto.silva5456@gmail.com>
2020-04-23 16:11:01 -0300

    image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './componentes/header';
 import Menu from './componentes/menu';
+import Api from './componentes/server/index';
+import Products from './componentes/Products';
+import Settings from './componentes/settings'
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Grid from "@material-ui/core/Grid";
 
-const App = () => (
-  <div className ="App">
-    <Header/>
-    <Menu/>
-  </div>
-);
+
+const useStyles = makeStyles(theme => ({
+  grid: {
+    padding: theme.spacing(0),
+    margin: theme.spacing(0),
+    border: 'none',
+    flexGrow: 0,
+  },
+
+}));
+
+function App() {
+  const classes = useStyles();
+  const [categories, setCategories] = useState([]);
+  const [Option, setOption] = useState(null);
+
+  useEffect(() => {
+    Api.get('categorias/')
+      .then((response) => setCategories(response.data))
+  }, []);
+
+  useEffect(() => {
+
+  }, [Option]);
+
+  function handleSelect(id) {
+    if (id === 'settings') {
+      setOption('settings');
+    }
+    else {
+      setOption(...categories.filter((category) => category._id === id));
+    }
+
+  }
+  function Select() {
+    if (Option === 'settings') return <Settings />;
+    else {
+      return <Products category={Option} />
+    };
+  }
+  return (
+    <div className={classes.grid}>
+      <Grid container spacing={0} className={classes.grid}>
+        <Header />
+      </Grid>
+      <Grid container className={classes.grid}>
+        <Grid item xs={3} className={classes.grid}>
+          <Menu handleSelectCategory={handleSelect} categories={categories} />
+        </Grid>
+        <Grid item xs >
+          <Select />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
 
 export default App;
+
--- src/componentes/Products/index.js
+++ src/componentes/Products/index.js
@@ -1,61 +1,142 @@
-import React from 'react';
 import MaterialTable from 'material-table'
+import React, { useState, useEffect } from 'react';
+import api from '../server';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    display: 'none',
+  },
+  root:{
+    diplay:'none'
+  }
+}));
 function CategoriesProducts({ category }) {
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+  //const [Columns, setColumns] = useState([]);
+  const [image, setImage] = useState('');
+
+
+  useEffect(() => {
+    if (category !== null) {
+      api.get(`categorias/${category._id}`)
+        .then((response) => setProducts(response.data.products))
+    }
+  }, [category]);
 
   if (category === null) {
     return null;
   }
 
+  function addProduct(product) {
+    return new Promise((resolve, reject) => {
+      console.log(image);
+      const data = new FormData();
+      data.append('name', product.name);
+      data.append('value', product.value);
+      data.append('quantity', product.quantity);
+      data.append('image', image);
+
+      api.post(`categorias/${category._id}/produtos`, data)
+        .then((response) => {
+          setProducts([...products, product]);
+          resolve();
+        })
+        .catch((error) => {
+          alert('Não foi possível adicionar o produto!');
+          console.log(error);
+          reject();
+        });
+    })
+  }
+
+  function editProduct(newProduct, oldProduct) {
+    return new Promise((resolve, reject) => {
+      api.put(`produtos/${oldProduct._id}`, newProduct)
+        .then((response) => {
+          setProducts([...products.filter((product) => product._id !== oldProduct._id), newProduct]);
+          resolve();
+        })
+        .catch((error) => {
+          alert('Erro ao editar produto!')
+          reject();
+        });
+    });
+  }
+
+  function deleteProduct(oldProduct) {
+    return new Promise((resolve, reject) => {
+      api.delete(`/produtos/${oldProduct._id}`)
+        .then((response) => {
+          setProducts(products.filter((product) => product._id !== oldProduct._id));
+          resolve();
+        })
+        .catch((error) => {
+          alert(`Não foi possível deletar o produto ${oldProduct.name}`);
+          reject();
+        });
+    });
+  }
   return (
     <div>
       <MaterialTable
-        columns={[
-          { title: 'Nome', field: 'name' },
-          { title: 'Valor', field: 'value' },
-          { title: 'Quantidade', field: 'quantity', type: 'numeric' },
-        ]}
-        data={category.products}
-        title={category.name}
-        options={{
-          search: true
+        localization={{
+          header: {
+            actions: 'Ações'
+          },
+          body: {
+            addTooltip: 'Adicionar Produto',
+            deleteTooltip: 'Deletar Produto',
+            editTooltip: 'Editar Produto',
+            editRow: {
+              deleteText:' Tem certeza que deseja deletar o produto?',
+              cancelTooltip:'Cancelar',
+              saveTooltip:'Deletar',
+
+            }
+            
+          }
         }}
+        columns={[{ title: 'Nome', field: 'name' },
+        {
+          title: 'Valor', field: 'value', type: 'currency', currencySetting: { currencyCode: 'BRL' }
+        },
+        { title: 'Quantidade', field: 'quantity', type: 'numeric', defaultSort: 'desc', },
+        {
+          title: 'Imagem do Produto',
+          field: 'image',
+          render: rowData => {
+            if (rowData.image) {
+              return <img src={rowData.image} alt={rowData.name} style={{ width: 80 }} />
+            }
+          },
+          editComponent: props => (
+            <div>
+              <input className={classes.input}
+                id="contained-button-file"
+                type="file" name="image" accept="image/png, image/jpeg, image/jpg"
+                onChange={e => setImage(e.target.files[0])}
+              />
+              <label htmlFor="contained-button-file">
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+              <label>{props.value}</label>
+            </div>
+          ),
+          type: 'date'
+        }]}
+        data={products}
+        title={category.name}
         editable={{
-          onRowAdd: newData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                {
-                  const data = this.state.data;
-                  data.push(newData);
-                  this.setState({ data }, () => resolve());
-                }
-                resolve()
-              }, 1000)
-            }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                {
-                  const data = this.state.data;
-                  const index = data.indexOf(oldData);
-                  data[index] = newData;
-                  this.setState({ data }, () => resolve());
-                }
-                resolve()
-              }, 1000)
-            }),
-          onRowDelete: oldData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                {
-                  let data = this.state.data;
-                  const index = data.indexOf(oldData);
-                  data.splice(index, 1);
-                  this.setState({ data }, () => resolve());
-                }
-                resolve()
-              }, 1000)
-            }),
+          onRowAdd: newProduct => addProduct(newProduct),
+          onRowUpdate: (newProduct, oldProduct) => editProduct(newProduct, oldProduct),
+          onRowDelete: product => deleteProduct(product),
         }}
       />
     </div>
