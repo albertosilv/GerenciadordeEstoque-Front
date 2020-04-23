6e3a9c32f0809fca523f067efbb767d9b3864704
Alberto <alberto.silva5456@gmail.com>
2020-04-23 15:56:48 -0300

    image

--- src/componentes/Products/index.js
+++ src/componentes/Products/index.js
@@ -1,9 +1,23 @@
 import MaterialTable from 'material-table'
 import React, { useState, useEffect } from 'react';
 import api from '../server';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    display: 'none',
+  },
+}));
 function CategoriesProducts({ category }) {
+  const classes = useStyles();
   const [products, setProducts] = useState([]);
+  //const [Columns, setColumns] = useState([]);
+  const [image, setImage] = useState('');
+
+
   useEffect(() => {
     if (category !== null) {
       api.get(`categorias/${category._id}`)
@@ -17,10 +31,12 @@ function CategoriesProducts({ category }) {
 
   function addProduct(product) {
     return new Promise((resolve, reject) => {
+      console.log(image);
       const data = new FormData();
       data.append('name', product.name);
       data.append('value', product.value);
       data.append('quantity', product.quantity);
+      data.append('image', image);
 
       api.post(`categorias/${category._id}/produtos`, data)
         .then((response) => {
@@ -29,6 +45,7 @@ function CategoriesProducts({ category }) {
         })
         .catch((error) => {
           alert('Não foi possível adicionar o produto!');
+          console.log(error);
           reject();
         });
     })
@@ -67,24 +84,50 @@ function CategoriesProducts({ category }) {
         localization={{
           header: {
             actions: 'Ações'
+          },
+          body: {
+            addTooltip: 'Adicionar Produto',
+            deleteTooltip: 'Deletar Produto',
+            editTooltip: 'Editar Produto',
+            editRow: {
+              deleteText:' Tem certeza que deseja deletar o produto?',
+              cancelTooltip:'Cancelar',
+              saveTooltip:'Deletar',
+
+            }
+            
           }
         }}
-        columns={[
-          { title: 'Nome', field: 'name' },
-          {
-            title: 'Valor', field: 'value', type: 'currency', currencySetting: { currencyCode: 'BRL' }
-          },
-          { title: 'Quantidade', field: 'quantity', type: 'numeric', defaultSort: 'desc', },
-          {
-            title: 'Imagem do Produto',
-            field: 'image',
-            render: rowData => {
-              if (rowData.image) {
-                return <img src={rowData.image} alt={rowData.name} style={{ width: 80 }} />
-              }
+        columns={[{ title: 'Nome', field: 'name' },
+        {
+          title: 'Valor', field: 'value', type: 'currency', currencySetting: { currencyCode: 'BRL' }
+        },
+        { title: 'Quantidade', field: 'quantity', type: 'numeric', defaultSort: 'desc', },
+        {
+          title: 'Imagem do Produto',
+          field: 'image',
+          render: rowData => {
+            if (rowData.image) {
+              return <img src={rowData.image} alt={rowData.name} style={{ width: 80 }} />
             }
-          }
-        ]}
+          },
+          editComponent: props => (
+            <div>
+              <input className={classes.input}
+                id="contained-button-file"
+                type="file" name="image" accept="image/png, image/jpeg, image/jpg"
+                onChange={e => setImage(e.target.files[0])}
+              />
+              <label htmlFor="contained-button-file">
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+              <label>{props.value}</label>
+            </div>
+          ),
+          type: 'date'
+        }]}
         data={products}
         title={category.name}
         editable={{
