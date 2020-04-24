2005a42ea610dbf2f3072dc8c9c4a3263c4e84c8
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-04-24 17:12:14 -0300

    add react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import Header from './componentes/header';
-import Menu from './componentes/menu';
-import Api from './componentes/server/index';
-import Products from './componentes/Products';
-import Settings from './componentes/settings'
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import Grid from "@material-ui/core/Grid";
-
-
-const useStyles = makeStyles(theme => ({
-  grid: {
-    padding: theme.spacing(0),
-    margin: theme.spacing(0),
-    border: 'none',
-    flexGrow: 0,
-  },
-
-}));
+import React from 'react';
+import { Route, useHistory, Switch, Redirect } from 'react-router-dom';
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./Auth/Auth";
 
 function App() {
-  const classes = useStyles();
-  const [categories, setCategories] = useState([]);
-  const [Option, setOption] = useState(null);
+  const history = useHistory();
+  const auth = new Auth(history);
 
-  useEffect(() => {
-    Api.get('categorias/')
-      .then((response) => setCategories(response.data))
-  }, []);
-
-  useEffect(() => {
-
-  }, [Option]);
-
-  function handleSelect(id) {
-    if (id === 'settings') {
-      setOption('settings');
-    }
-    else {
-      setOption(...categories.filter((category) => category._id === id));
-    }
-
-  }
-  function Select() {
-    if (Option === 'settings') return <Settings />;
-    else {
-      return <Products category={Option} />
-    };
-  }
   return (
-    <div className={classes.grid}>
-      <Grid container spacing={0} className={classes.grid}>
-        <Header />
-      </Grid>
-      <Grid container className={classes.grid}>
-        <Grid item xs={3} className={classes.grid}>
-          <Menu handleSelectCategory={handleSelect} categories={categories} />
-        </Grid>
-        <Grid item xs >
-          <Select />
-        </Grid>
-      </Grid>
-    </div>
-  );
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={(props) => <Login auth={auth} {...props} />}
+      />
+      <Route
+        path='/dashboard'
+        render={(props) => auth.isAuthenticated() ? <Dashboard {...props} auth={auth} /> : <Redirect to='/' />}
+      />
+    </Switch>
+  )
 }
+
+//1587760940710
 
 export default App;
 
