9771be0a74f882578773f27a50c62a75d77ee484
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-04-27 10:33:40 -0300

    Fix logout

--- src/components/ExitIcon/index.js
+++ src/components/ExitIcon/index.js
@@ -6,7 +6,7 @@ const Exit = ({ auth }) => {
         <GoogleLogout
             clientId="86796554590-gt3o70mtm8qq35lebu0enu6k7nvc2arg.apps.googleusercontent.com"
             buttonText="Sair"
-            onLogoutSuccess={auth.logout}
+            onLogoutSuccess={() => auth.logout()}
             theme="dark"
         />
     )
